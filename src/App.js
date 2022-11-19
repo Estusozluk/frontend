6ba241daf0b3f53c1cf10b73f048925b07160631
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2022-11-19 10:20:02 +0300

    implement navbar & aside

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import AsideBar from './components/AsideBar/AsideBar';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <BrowserRouter>
+      <Navbar />
+      <AsideBar />
+
+     
+      <Routes>
+        <Route path='/' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Navbar.css'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className='navigationBar'>
+      <div className='navLogo'>
+        <p>estüsözlük</p>
+      </div>
+
+      <nav className='topBar'>
+        <ul>
+          <Link to='/login'>
+          <li>giriş</li>
+          </Link>
+
+          <Link to='/register'>
+          <li>kayıt ol</li>
+          </Link>
+        </ul>
+      </nav>
+
+      <nav className='bottomBar'>
+        <ul>
+          <li>#gündem</li>
+          <li>#spor</li>
+          <li>#siyaset</li>
+          <li>#teknoloji</li>
+          <li>#estü</li>
+        </ul>
+      </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
